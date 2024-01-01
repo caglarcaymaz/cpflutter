bc258fad9c048e17503497e0f469519a7599a804
caglarcaymaz92 <90869301+caglarcaymaz92@users.noreply.github.com>
2024-01-01 20:41:26 +0300

    file creation

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+// this is the cflutter practice
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
