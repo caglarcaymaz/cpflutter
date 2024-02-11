a18b3f447e4e8365bd75cb839209670fe72df176
caglarcaymaz92 <90869301+caglarcaymaz92@users.noreply.github.com>
2024-02-11 23:59:47 +0300

    User model

---
+++ lib/models/users.dart
@@ -0,0 +1,15 @@
+class User{
+  String name;
+  String profilePhoto;
+  String email;
+  String uid;
+}
+User(
+  {
+    required this.name;
+    required this.email;
+    required this.uid;
+    required this.profilePhoto;
+    
+  }
+)
