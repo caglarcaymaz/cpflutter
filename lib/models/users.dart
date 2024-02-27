7b86accc40e1f861558798187000a838675f1447
caglarcaymaz92 <90869301+caglarcaymaz92@users.noreply.github.com>
2024-02-27 23:24:22 +0300

    ToJson and fromSnapshot for user model

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User{
   String? name;
   String? profilePhoto;
@@ -15,6 +17,13 @@ User(
 );
 Map<String,dynamic> toJson=>{
   "name": name,
-  "profilephoto: profilephoto"
+  "profilePhoto": profilePhoto,
+  "email":email,
+  "uid":uid,
+};
+static User fromSnap(DocumentSnapshot snap){
+  var snapshot= snap.data() as Map<String,dynamic>;
+  return User(email: snapshot['email'],profilePhoto: snapshot['profilePhoto'],
+  uid: snapshot['uid'],name: snapshot['name'],);
 }
 }
