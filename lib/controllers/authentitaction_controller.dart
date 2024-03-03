6835075d0e12a05dab1ddd420020ff07edf4d753
caglarcaymaz92 <90869301+caglarcaymaz92@users.noreply.github.com>
2024-03-03 23:39:45 +0300

    user imported to authcontroller with required fileds

--- lib/controllers/authentitaction_controller.dart
+++ lib/controllers/authentitaction_controller.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:cflutter/models/users.dart' as models;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 
 import '../constants.dart';
+
 class AuthController extends GetxController {
   Future<String> _uploadToStorage(File image) async{
     
@@ -19,8 +21,11 @@ class AuthController extends GetxController {
       if(username.isNotEmpty && email.isNotEmpty && password.isNotEmpty && image!=null){
      
       UserCredential cred =   await  firebase_auth.createUserWithEmailAndPassword(email: email,password: password);
-      }
+      
+      
     String downloadUrl=  await  _uploadToStorage(image!);
+    models.User user = models.User(name: username, email : email , uid : cred.user!.uid , profilePhoto: downloadUrl);
+      }
     }
     catch(e){
       Get.snackbar("Error Occured while creating an account", 
